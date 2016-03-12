d30265a2f053261b6cbd93d2353d0a10cd09980a
SÁROSSI Ders <bono@bono.hu>
2016-03-12 14:57:35 +0100

    removed commented documentation.

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -11,32 +11,6 @@ public class Application
 
     public static void main(String[] args)
     {
-        // List<Coordinate> coordinates = snake.getBodyItems();
-        // Coordinate coordinate = snake.getHeadCoordinate();
-        // String name = snake.getName();
-        // Coordinate coordinate = snake.getTailCoordinate();
-        // int length = snake.length();
-        // boolean occupies = snake.occupies(nextCoordinate);
-
-        // Coordinate coordinate = arena.generateRandomFreeCoordinate();
-        // List<Food> foods = arena.getFood();
-        // Coordinate coordinate = arena.getMaxCoordinate();
-        // List<Snake> snakes = arena.getSnakes();
-        // boolean isFood = arena.isFood(nextCoordinate);
-        // boolean isOccupied = arena.isOccupied(nextCoordinate);
-        // Coordinate coordinate = arena.nextCoordinate(coordinate, direction);
-
-        // int x = coordinate.getX();
-        // int y = coordinate.getY();
-        // int distance = coordinate.minDistance(otherCoordinate,
-        // maxCoordinate);
-        // Coordinate coordinate = coordinate.nextCoordinate(direction);
-        // int distance = coordinate.standardDistance(otherCoordinate);
-        // Coordinate coordinate = coordinate.truncLimits(maxCoordinates);
-
-        // Coordinate coordinate = food.getCoordinate();
-        // boolean occupies = food.occupies(coordinate);
-
         System.setSecurityManager(new SecurityManager());
 
         ModifiableArena arena = new ModifiableArena();

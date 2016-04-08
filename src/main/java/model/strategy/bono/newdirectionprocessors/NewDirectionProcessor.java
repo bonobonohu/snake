91d1af5191e58eacb4b52b629be5af5939c7126d
SÁROSSI Ders <bono@bono.hu>
2016-04-08 21:23:28 +0200

    reorder.

--- src/main/java/model/strategy/bono/newdirectionprocessors/NewDirectionProcessor.java
+++ src/main/java/model/strategy/bono/newdirectionprocessors/NewDirectionProcessor.java
@@ -1,22 +1,22 @@
 package model.strategy.bono.newdirectionprocessors;
 
 import model.Direction;
-import model.strategy.bono.directionhandlers.DirectionContainer;
 import model.strategy.bono.directionhandlers.BlockingDirectionContainer;
+import model.strategy.bono.directionhandlers.DirectionContainer;
 
 public abstract class NewDirectionProcessor
 {
     protected BlockingDirectionContainer blockingDirections;
-    protected DirectionContainer<Direction> allValidDirections;
     protected DirectionContainer<Direction> equivalentBestDirections;
+    protected DirectionContainer<Direction> allValidDirections;
 
     public NewDirectionProcessor(DependencyProvider dependencyProvider)
     {
         this.blockingDirections = dependencyProvider
                 .getBlockingDirectionsDataHandler();
-        this.allValidDirections = dependencyProvider.getAllValidDirections();
         this.equivalentBestDirections = dependencyProvider
                 .getEquivalentBestDirections();
+        this.allValidDirections = dependencyProvider.getAllValidDirections();
     }
 
     public abstract Direction getNewDirection();

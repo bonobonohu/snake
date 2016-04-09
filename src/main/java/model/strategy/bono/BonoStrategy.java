9e2adb8fe9b5eb1ea799eff6a136178089ad1690
SÁROSSI Ders <bono@bono.hu>
2016-04-09 18:23:16 +0200

    extracted to its own class.

--- src/main/java/model/strategy/bono/BonoStrategy.java
+++ src/main/java/model/strategy/bono/BonoStrategy.java
@@ -12,7 +12,6 @@ import model.Snake;
 import model.strategy.SnakeStrategy;
 import model.strategy.bono.directionhandlers.BlockingDirectionContainer;
 import model.strategy.bono.directionhandlers.SimpleDirectionContainer;
-import model.strategy.bono.distanceprocessors.DistanceProcessor;
 import model.strategy.bono.newdirectionprocessors.ByBlockingDistances;
 import model.strategy.bono.newdirectionprocessors.ByEquivalentBestDirections;
 import model.strategy.bono.newdirectionprocessors.ByFreeEquivalentBestDirections;
@@ -60,13 +59,17 @@ public class BonoStrategy implements SnakeStrategy
 
         Map<Integer, SimpleDirectionContainer<Direction>> distancesToFood = new TreeMap<>();
 
+        BlockingDirectionProcessor blockingDirectionProcessor = new BlockingDirectionProcessor(
+                snake, arena);
+
         BlockingDirectionContainer blockingDirections = new BlockingDirectionContainer();
 
         SimpleDirectionContainer<Direction> equivalentBestDirections = new SimpleDirectionContainer<>();
         SimpleDirectionContainer<Direction> allValidDirections = new SimpleDirectionContainer<>();
 
         int stepForward = 0;
-        blockingDirections = processBlockingDirections(stepForward);
+        blockingDirections = blockingDirectionProcessor
+                .process(stepForward);
 
         distancesToFood = processDistancesToFood();
 
@@ -108,126 +111,6 @@ public class BonoStrategy implements SnakeStrategy
         }
 
         return distancesToFood;
-    }
-
-    private BlockingDirectionContainer processBlockingDirections(
-            int stepForward)
-    {
-        BlockingDirectionContainer blockingDirections = new BlockingDirectionContainer();
-
-        for (Direction actualDirection : Direction.values()) {
-            Coordinate nextCoordinate = arena
-                    .nextCoordinate(actualHeadCoordinate, actualDirection);
-
-            if (!arena.isOccupied(nextCoordinate)) {
-                Coordinate coordinateToInvestigate = actualHeadCoordinate;
-
-                for (int i = 0; i < 49; i++) {
-                    coordinateToInvestigate = arena.nextCoordinate(
-                            coordinateToInvestigate, actualDirection);
-
-                    if (arena.isOccupied(coordinateToInvestigate)) {
-                        Snake blockingSnake = getBlockingSnake(
-                                coordinateToInvestigate);
-
-                        if (isValidBlock(blockingSnake)) {
-                            int blockingTailLength = getBlockingTailLength(
-                                    blockingSnake, coordinateToInvestigate);
-
-                            DistanceProcessor distanceProcessor = DistanceProcessor
-                                    .getDistanceProcessor(actualDirection);
-                            int distanceToBlock = distanceProcessor.getDistance(
-                                    actualHeadCoordinate,
-                                    coordinateToInvestigate, maxCoordinate);
-
-                            if (isBlockingRisk(blockingTailLength,
-                                    distanceToBlock + stepForward)) {
-                                blockingDirections.putData(actualDirection,
-                                        coordinateToInvestigate,
-                                        distanceToBlock + stepForward);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println("Blocking Directions: " + blockingDirections);
-
-        return blockingDirections;
-    }
-
-    private Snake getBlockingSnake(Coordinate blockingCoordinate)
-    {
-        Snake blockingSnake = null;
-
-        List<Snake> snakes = arena.getSnakes();
-        for (Snake actualSnake : snakes) {
-            List<Coordinate> bodyItems = actualSnake.getBodyItems();
-
-            for (Coordinate bodyItem : bodyItems) {
-                if (bodyItem.equals(blockingCoordinate)) {
-                    blockingSnake = actualSnake;
-                }
-            }
-        }
-
-        return blockingSnake;
-    }
-
-    private boolean isValidBlock(Snake blockingSnake)
-    {
-        boolean validBlock = false;
-
-        if (blockingSnake.equals(snake)) {
-            boolean allXsAreTheSame = true;
-            boolean allYsAreTheSame = true;
-
-            for (Coordinate actualBodyItem : snake.getBodyItems()) {
-                if (actualBodyItem.getX() != actualHeadCoordinate.getX()) {
-                    allXsAreTheSame = false;
-                }
-                if (actualBodyItem.getY() != actualHeadCoordinate.getY()) {
-                    allYsAreTheSame = false;
-                }
-            }
-
-            if ((!allXsAreTheSame && !allYsAreTheSame)
-                    || (allXsAreTheSame && snake.length() == arena
-                            .getMaxCoordinate().getY())
-                    || (allYsAreTheSame && snake.length() == arena
-                            .getMaxCoordinate().getX())) {
-                validBlock = true;
-            }
-        } else {
-            validBlock = true;
-        }
-
-        return validBlock;
-    }
-
-    private int getBlockingTailLength(Snake blockingSnake,
-            Coordinate nextCoordinateToInvestigate)
-    {
-        boolean reachedTheBlockingPart = false;
-        int blockingTailLength = 0;
-
-        for (Coordinate actualBodyItem : blockingSnake.getBodyItems()) {
-            if (actualBodyItem.equals(nextCoordinateToInvestigate)) {
-                reachedTheBlockingPart = true;
-            }
-
-            if (reachedTheBlockingPart) {
-                blockingTailLength++;
-            }
-        }
-
-        return blockingTailLength;
-    }
-
-    private boolean isBlockingRisk(int blockingTailLength, int distanceToBlock)
-    {
-        return blockingTailLength >= distanceToBlock;
     }
 
     private SimpleDirectionContainer<Direction> processEquivalentBestDirections(

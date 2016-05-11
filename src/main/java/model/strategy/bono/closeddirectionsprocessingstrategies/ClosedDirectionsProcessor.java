7b6e2384569cff2e2a22e0fce9c4717672cc64cc
SÁROSSI Ders <bono@bono.hu>
2016-05-11 10:33:17 +0200

    whitespace.

--- src/main/java/model/strategy/bono/closeddirectionsprocessingstrategies/ClosedDirectionsProcessor.java
+++ src/main/java/model/strategy/bono/closeddirectionsprocessingstrategies/ClosedDirectionsProcessor.java
@@ -9,7 +9,6 @@ public abstract class ClosedDirectionsProcessor
 {
     public static ClosedDirectionsProcessor getStrategy()
     {
-
         return new MinimumsExceptZerosAndZerosStrategy();
     }
 

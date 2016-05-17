43dea531cfc40c305469a047e3782de93ab60c0b
SÁROSSI Ders <bono@bono.hu>
2016-05-17 12:38:29 +0200

    this way, stupid!

--- src/main/java/controller/Application.java
+++ src/main/java/controller/Application.java
@@ -6,33 +6,6 @@ import model.strategy.bono.BonoStrategy;
 
 public class Application
 {
-    /**
-     *   <repositories>
-     <repository>
-     <snapshots>
-     <enabled>false</enabled>
-     </snapshots>
-     <id>central</id>
-     <name>Central Repository</name>
-     <url>http://repo.maven.apache.org/maven2</url>
-     </repository>
-     </repositories>
-
-     <pluginRepositories>
-     <pluginRepository>
-     <releases>
-     <updatePolicy>never</updatePolicy>
-     </releases>
-     <snapshots>
-     <enabled>false</enabled>
-     </snapshots>
-     <id>central</id>
-     <name>Central Repository</name>
-     <url>http://repo.maven.apache.org/maven2</url>
-     </pluginRepository>
-     </pluginRepositories>
-     * @param args
-     */
     public static void main(String[] args)
     {
         System.setSecurityManager(new SecurityManager());

aa8610750f5bd4fe54fcca07c5dcab34cf70ae18
Ders Sarossi <ders.sarossi@monese.com>
2020-04-25 20:18:42 +0300

    Update dependencies, Springize, Kotlinize

---
+++ src/main/kotlin/co/electric/snake/KotlinApplication.kt
@@ -0,0 +1,11 @@
+package co.electric.snake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApplication>(*args)
+}

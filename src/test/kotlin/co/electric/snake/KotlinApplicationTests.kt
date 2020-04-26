411fe5252b20cbcb71a1e914fa4edd212279364a
Ders Sarossi <ders.sarossi@monese.com>
2020-04-26 04:16:49 +0300

    Set up KotlinApplicationTests with headless
    ... property properly set.

--- src/test/kotlin/co/electric/snake/KotlinApplicationTests.kt
+++ src/test/kotlin/co/electric/snake/KotlinApplicationTests.kt
@@ -2,8 +2,10 @@ package co.electric.snake
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ContextConfiguration(loader = (HeadlessSpringBootContextLoader::class))
 class KotlinApplicationTests {
 
     @Test

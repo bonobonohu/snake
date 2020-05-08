e3b6f32b7e48c9e1b882f48f175f1688a99add1d
Ders Sarossi <ders.sarossi@monese.com>
2020-05-08 21:42:52 +0300

    Kotlinize FreeDirectionsProcessor

---
+++ snake-strategy-bono/src/main/kotlin/co/electric/snake/strategy/bonostrategy/directionprocessor/filtereddirections/FreeDirectionsProcessor.kt
@@ -0,0 +1,26 @@
+package co.electric.snake.strategy.bonostrategy.directionprocessor.filtereddirections
+
+import co.electric.snake.framework.model.Arena
+import co.electric.snake.framework.model.Coordinate
+import co.electric.snake.framework.model.Direction
+import co.electric.snake.strategy.bonostrategy.SimpleDirectionContainer
+import org.slf4j.LoggerFactory
+
+class FreeDirectionsProcessor {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(FreeDirectionsProcessor::class.java)
+    }
+
+    fun getDirections(arena: Arena, actualHeadCoordinate: Coordinate): SimpleDirectionContainer {
+        val freeDirections = SimpleDirectionContainer()
+        Direction.values().forEach {
+            if (!arena.isOccupied(arena.nextCoordinate(actualHeadCoordinate, it))) {
+                freeDirections.add(it)
+            }
+        }
+        LOG.info("Free Directions: $freeDirections")
+        return freeDirections
+    }
+
+}

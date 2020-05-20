c2a3bf5bc9721a3ed3f0b2396624f0349cc282a5
Ders Sarossi <bono@bono.hu>
2020-05-20 23:28:23 +0300

    Add test for ByKispalEsABorz class

---
+++ snake-strategy-bono/src/test/kotlin/co/electric/snake/strategy/bonostrategy/newdirectionprocessor/ByKispalEsABorzTest.kt
@@ -0,0 +1,27 @@
+package co.electric.snake.strategy.bonostrategy.newdirectionprocessor
+
+import co.electric.snake.framework.model.Direction
+import co.electric.snake.strategy.bonostrategy.BlockingDirectionContainer
+import co.electric.snake.strategy.bonostrategy.SimpleDirectionContainer
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class ByKispalEsABorzTest {
+
+    private val underTest = ByKispalEsABorz()
+
+    @Test
+    fun testProcessShouldReturnSouthDirectionOptionalWhenCalled() {
+        // GIVEN
+        val filteredDirections = SimpleDirectionContainer()
+        val equivalentBestDirections = SimpleDirectionContainer()
+        val blockingDirections = BlockingDirectionContainer()
+        val expected = Optional.of(Direction.SOUTH)
+        // WHEN
+        val actual = underTest.process(filteredDirections, equivalentBestDirections, blockingDirections)
+        // THEN
+        assertEquals(expected, actual)
+    }
+
+}

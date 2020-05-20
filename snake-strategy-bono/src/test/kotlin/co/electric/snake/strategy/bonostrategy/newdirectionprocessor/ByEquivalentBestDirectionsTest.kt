37375e94ddedeff6b5ba3797793fd20fd10bde4b
Ders Sarossi <bono@bono.hu>
2020-05-21 00:15:09 +0300

    Add test for ByEquivalentBestDirections class

---
+++ snake-strategy-bono/src/test/kotlin/co/electric/snake/strategy/bonostrategy/newdirectionprocessor/ByEquivalentBestDirectionsTest.kt
@@ -0,0 +1,39 @@
+package co.electric.snake.strategy.bonostrategy.newdirectionprocessor
+
+import co.electric.snake.framework.model.Direction
+import co.electric.snake.strategy.bonostrategy.BlockingDirectionContainer
+import co.electric.snake.strategy.bonostrategy.SimpleDirectionContainer
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ByEquivalentBestDirectionsTest {
+
+    private val underTest = ByEquivalentBestDirections()
+
+    @Test
+    fun testProcessShouldReturnRandomElementFromEquivalentBestDirectionsWhenItContainsMultipleElements() {
+        // GIVEN
+        val expecteds = setOf(Direction.SOUTH, Direction.WEST)
+        val filteredDirections = SimpleDirectionContainer()
+        val equivalentBestDirections = SimpleDirectionContainer(expecteds)
+        val blockingDirections = BlockingDirectionContainer()
+        // WHEN
+        val actual = underTest.process(filteredDirections, equivalentBestDirections, blockingDirections)
+        // THEN
+        Assertions.assertThat(actual.get()).isIn(expecteds)
+    }
+
+    @Test
+    fun testProcessShouldReturnTheContainedElementFromEquivalentBestDirectionsWhenItContainsExactlyOneSingleElement() {
+        // GIVEN
+        val expected = Direction.WEST
+        val filteredDirections = SimpleDirectionContainer()
+        val equivalentBestDirections = SimpleDirectionContainer(setOf(expected))
+        val blockingDirections = BlockingDirectionContainer()
+        // WHEN
+        val actual = underTest.process(filteredDirections, equivalentBestDirections, blockingDirections)
+        // THEN
+        Assertions.assertThat(actual.get()).isEqualTo(expected)
+    }
+
+}

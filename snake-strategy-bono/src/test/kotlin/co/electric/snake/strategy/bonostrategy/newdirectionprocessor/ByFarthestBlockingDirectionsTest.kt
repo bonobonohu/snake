9236a34acfa736b2035871a9a8263c2c46a2acbc
Ders Sarossi <bono@bono.hu>
2020-05-21 15:42:39 +0300

    Add test for ByFarthestBlockingDirections class

---
+++ snake-strategy-bono/src/test/kotlin/co/electric/snake/strategy/bonostrategy/newdirectionprocessor/ByFarthestBlockingDirectionsTest.kt
@@ -0,0 +1,48 @@
+package co.electric.snake.strategy.bonostrategy.newdirectionprocessor
+
+import co.electric.snake.framework.model.Coordinate
+import co.electric.snake.framework.model.Direction
+import co.electric.snake.strategy.bonostrategy.BlockingDirectionContainer
+import co.electric.snake.strategy.bonostrategy.SimpleDirectionContainer
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class ByFarthestBlockingDirectionsTest {
+
+    private val underTest = ByFarthestBlockingDirections()
+
+    @Test
+    fun testProcessShouldReturnARandomFarthestBlockingDirectionWhenThereAreMultipleFarthestElements() {
+        // GIVEN
+        val expecteds = setOf(Direction.WEST, Direction.EAST)
+        val filteredDirections = SimpleDirectionContainer()
+        val equivalentBestDirections = SimpleDirectionContainer(expecteds)
+        val blockingDirections = BlockingDirectionContainer()
+        blockingDirections.putData(Direction.WEST, Coordinate(1, 1), 11)
+        blockingDirections.putData(Direction.EAST, Coordinate(23, 1), 11)
+        blockingDirections.putData(Direction.NORTH, Coordinate(11, 3), 2)
+        blockingDirections.putData(Direction.SOUTH, Coordinate(11, 49), 2)
+        // WHEN
+        val actual = underTest.process(filteredDirections, equivalentBestDirections, blockingDirections)
+        // THEN
+        Assertions.assertThat(actual.get()).isIn(expecteds)
+    }
+
+    @Test
+    fun testProcessShouldReturnTheOnlyFarthestBlockingDirectionWhenThereIsOnlyOneFarthestElement() {
+        // GIVEN
+        val expected = Direction.WEST
+        val filteredDirections = SimpleDirectionContainer()
+        val equivalentBestDirections = SimpleDirectionContainer()
+        val blockingDirections = BlockingDirectionContainer()
+        blockingDirections.putData(expected, Coordinate(1, 1), 11)
+        blockingDirections.putData(Direction.EAST, Coordinate(22, 1), 10)
+        blockingDirections.putData(Direction.NORTH, Coordinate(11, 3), 2)
+        blockingDirections.putData(Direction.SOUTH, Coordinate(11, 49), 2)
+        // WHEN
+        val actual = underTest.process(filteredDirections, equivalentBestDirections, blockingDirections)
+        // THEN
+        Assertions.assertThat(actual.get()).isEqualTo(expected)
+    }
+
+}

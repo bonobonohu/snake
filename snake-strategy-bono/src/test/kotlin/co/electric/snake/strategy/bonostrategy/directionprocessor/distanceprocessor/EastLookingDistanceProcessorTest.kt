17128e197ef0d3fdf3a4dd638bb0e0080cf7c60a
Ders Sarossi <bono@bono.hu>
2020-05-22 21:38:56 +0300

    Add test for EastLookingDistanceProcessor class

---
+++ snake-strategy-bono/src/test/kotlin/co/electric/snake/strategy/bonostrategy/directionprocessor/distanceprocessor/EastLookingDistanceProcessorTest.kt
@@ -0,0 +1,49 @@
+package co.electric.snake.strategy.bonostrategy.directionprocessor.distanceprocessor
+
+import co.electric.snake.framework.model.Coordinate
+import co.electric.snake.framework.model.Direction
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class EastLookingDistanceProcessorTest {
+
+    companion object {
+
+        private val MAX_COORDINATE = Coordinate(50, 50)
+
+    }
+
+    private val underTest = EastLookingDistanceProcessor()
+
+    @Test
+    fun getDistanceShouldReturnEmptyOptionalWhenDirectionIsNotEast() {
+        // GIVEN
+        val expected = Optional.empty<Int>()
+        // WHEN
+        val actual = underTest.getDistance(Direction.WEST, Coordinate(1, 1), Coordinate(11, 1), MAX_COORDINATE)
+        // THEN
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun getDistanceShouldReturnCorrectResultWhenDistanceProcessedDirectly() {
+        // GIVEN
+        val expected = Optional.of(10)
+        // WHEN
+        val actual = underTest.getDistance(Direction.EAST, Coordinate(1, 1), Coordinate(11, 1), MAX_COORDINATE)
+        // THEN
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun getDistanceShouldReturnCorrectResultWhenDistanceProcessedClipped() {
+        // GIVEN
+        val expected = Optional.of(10)
+        // WHEN
+        val actual = underTest.getDistance(Direction.EAST, Coordinate(41, 1), Coordinate(1, 1), MAX_COORDINATE)
+        // THEN
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+}
